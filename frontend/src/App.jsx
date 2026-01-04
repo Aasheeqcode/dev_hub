85dc75f59fce0514c0a3b1745867ef5f09e4d9e4
AasheeqCode <aasheeq06@gmail.com>
2026-01-04 13:11:53 +0530

    Initial commit - Complete Frontend UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+
+// Import your pages
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import HomePage from "./pages/HomePage"; // Import the new Home Page
-import { CssBaseline } from "@mui/material";
+import HomePage from "./pages/HomePage";
+import LandingPage from "./pages/LandingPage";
+import MeetHome from "./pages/MeetHome";    // <-- NEW IMPORT
+import DevStudio from "./pages/DevStudio";  // <-- NEW IMPORT
 
 function App() {
   return (
@@ -10,10 +15,20 @@ function App() {
       <BrowserRouter>
         <CssBaseline />
         <Routes>
+          {/* Default Redirect to Login */}
           <Route path="/" element={<Navigate to="/login" />} />
+          
+          {/* Auth Routes */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/home" element={<HomePage />} /> {/* NEW ROUTE */}
+          
+          {/* Feature Routes */}
+          <Route path="/home" element={<LandingPage />} />
+          <Route path="/meet" element={<MeetHome />} />        {/* <-- NEW ROUTE */}
+          <Route path="/devstudio" element={<DevStudio />} />  {/* <-- NEW ROUTE */}
+          
+          {/* Placeholder for others */}
+          <Route path="/contests" element={<h1 style={{color:"white", padding:"2rem"}}>Contests Coming Soon</h1>} />
         </Routes>
       </BrowserRouter>
     </div>
